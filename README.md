52bd4bc2a8edf92c5a8d61ffd7902eca083e59a7
Anne Lhote <anne.lhote@gmail.com>
2016-08-18 16:21:36 +0200

    Comment and explain the conversion from JSON to SQL

--- README.md
+++ README.md
@@ -2,6 +2,27 @@
 Create a convert from JSON format to SQL format to import data in Coral
 
 
+## Logical
+- "type" field from json file will not be preserved.
+- "access_type" field from json file will correspond to "Access" tab, "Authentication Type" field.
+- "ID" field from json file will correspond to "Product" tab, "Record ID" field.
+- "DESCRIPTION" field from json file will correspond to "Product" tab, "English description" field.
+- "LABEL" field from json file will not be preserved.
+- "TITLE" field from json file will correspond to header.
+- "Lang" field from json file will correspond to "Product" tab, "Language" field.
+- "URL\_REMOTE\_RES", "URL\_LOCAL\_RES" and "URL\_FREE\_RES" fields from json file will correspond to header "Product" tab, "Resource URL" field. The first mentionned field will have the priority.
+- "RELEVANCE" field from json file will not be preserved.
+- "CLASSEMENT_TYPE" field from json file will correspond to "Product" tab, "Type" field.
+- "ANALYTICS_ID" field from json file will not be preserved.
+- "short_description" field from json file will not be preserved.
+- "access_right" field from json file will not be preserved.
+- "label" field from json file will not be preserved.
+- "title" field from json file will not be preserved.
+- "az" field from json file will not be preserved.
+- "lang" field from json file will not be preserved.
+- "category" field from json file will correspond to "Product" tab, "Subjects" array.
+
+
 ## Licenses
 [LGPL V3.0](http://www.gnu.org/licenses/lgpl.txt "LGPL V3.0")
 
--- json2sql.py
+++ json2sql.py
@@ -1,6 +1,6 @@
 # !/usr/bin/env python
 # -*- coding: utf8 -*-
-# Convert  a json file to an sql inser statement
+# Convert  a json file to an sql insert statement
 # Execution example : python json2sql.py
 
 
@@ -8,33 +8,104 @@
 # Libs
 #
 
-import time
 import json
+import logging
+import os
+import sys
+import time
 
 
 #
 # Config
 #
 
-json_fr_file = 'data_fr.json'
-json_en_file = 'data_en.json'
+json_fr_file = 'data/data_fr.json'
+json_en_file = 'data/data_en.json'
 result_file = 'items.sql'
-table_name = 'Resource'
+resource_table_name = 'Resource'
+lang_table_name = 'ResourceLanguage'
+type_table_name = 'ResourceTypeLink'
+subject_table_name = 'ResourceSubject'
 admin_login = 'coral'
+languages = {'en' : 1, 'fr' : 2, 'es' : 3, 'pt' : 4, 'de' : 5, 'multi' : 6}
+types = {'reference tools' : 1, 'language dictionaries' : 2, 'e-books' : 3, 'press' : 4, 'e-journals' : 5, 'statistical data' : 6, 'financial data' : 7, 'multimedia' : 8, 'industry profiles' : 9, 'country profiles' : 10, 'maps' : 11, 'working papers' : 12, 'dissertations & theses' : 13, 'official publications' : 14, 'open access resources' : 15, 'national licenses' : 16, 'search engines' : 17, 'tools' : 18}
+subjects = {'News' : 1, 'Law' : 2 , 'Economics' : 3, 'Environment' : 4, 'Finance' : 5, 'Management' : 6, 'History' : 7, 'International Relations' : 8, 'Science' : 9, 'Political Science' : 10, 'Languages' : 11, 'Literature' : 12, 'Sociology' : 13, 'Philosophy' : 14, 'Ethnology' : 15, 'Religion' : 16, 'Arts' : 17, 'Education' : 18, 'Psychology' : 19, 'Geography' : 20}
+accesses = {'local access only' : 1, 'remote access only' : 2, 'local and remote access' : 3, 'free access' : 4, 'restricted access' : 5}
 
 
 #
 # Functions
 #
 
-def insertItem(item) :
+def mysqlFormat(s) :
+	s = s.replace('\'', '\\\'')
+	s = s.replace('\r\n', '')
+	s = s.strip()
+	return s
+
+def getUrl(item) :
+	if 'URL_REMOTE_RES' in item.keys() :
+		return item['URL_REMOTE_RES'].strip()
+	elif 'URL_LOCAL_RES' in item.keys() :
+		return item['URL_LOCAL_RES'].strip()
+	elif 'URL_FREE_RES' in item.keys() :
+		return item['URL_FREE_RES'].strip()
+	else :
+		return ''
+
+def insertFrenchItem(item) :
 	insert = ''
-	insert += 'INSERT INTO ' + table_name + ' (createDate, createLoginID, titleText, descriptionText) VALUES ('
-	insert += '\'' + time.strftime("%Y-%m-%d") + '\', '
-	insert += '\'' + admin_login + '\', '
-	insert += item['TITLE'] + ', '
-	insert += item['DESCRIPTION'] + ', '
-	insert += ');\n'
+	insert += 'UPDATE ' + resource_table_name
+	insert += ' SET titleText_fr = \'' + mysqlFormat(item['TITRE']) + '\','
+	insert += ' descriptionText_fr = \'' + mysqlFormat(item['DESCRIPTION']) + '\''
+	insert += ' WHERE resourceID = ' + item['ID'] + ';\n'
+	return insert
+
+def insertEnglishItem(item) :
+	insert = ''
+	if len(item) == 18 or len(item) == 19 :
+		if item['DESCRIPTION'] is None :
+			logging.error('The discription of this item is empty : ' + item['ID']);
+		insert += 'INSERT INTO ' + resource_table_name + ' (resourceID, createDate, createLoginID, titleText, descriptionText, resourceURL) VALUES ('
+		insert += item['ID'] + ', '
+		insert += '\'' + time.strftime("%Y-%m-%d") + '\', '
+		insert += '\'' + admin_login + '\', '
+		insert += '\'' + mysqlFormat(item['TITLE']) + '\', '
+		insert += '\'\', ' if item['DESCRIPTION'] is None else '\'' + mysqlFormat(item['DESCRIPTION']) + '\', '
+		insert += '\'' + getUrl(item) + '\''
+		insert += ');\n'
+		insert += 'INSERT INTO ' + lang_table_name + ' (resourceId, languageId) VALUES ('
+		insert += item['ID'] + ', '
+		insert += str(languages[item['LANG']])
+		insert += ');\n'
+		# Link a type to a resource
+		if item['CLASSEMENT_TYPE'] in types.keys() :
+			insert += 'INSERT INTO ' + type_table_name + ' (resourceId, resourceTypeId) VALUES ('
+			insert += item['ID'] + ', '
+			insert += str(types[item['CLASSEMENT_TYPE']])
+			insert += ');\n'
+		else :
+			if item['CLASSEMENT_TYPE'] not in ['databases', 'dictionaries, encyclopedias'] :
+				logging.error('This type doesn\'t exist : ' + item['CLASSEMENT_TYPE'] + '.')
+		# Link subjects to a resource
+		for category in item['category'] :
+			if category in subjects.keys() :
+				insert += 'INSERT INTO ' + subject_table_name + ' (resourceId, generalDetailSubjectLinkID) VALUES ('
+				insert += item['ID'] + ', '
+				insert += str(subjects[category])
+				insert += ');\n'
+			else :
+				if category not in ['Test', 'Economics & Finance', 'Social Sciences', 'Statistics', 'Reference'] :
+					logging.error('This category doesn\'t exist : ' + category + ' in item ' + item['ID'] + '.')
+		# Set access type of a resource
+		if item['access_type'][0] in accesses.keys() :
+			insert += 'UPDATE ' + resource_table_name
+			insert += ' SET authenticationTypeID = \'' + str(accesses[item['access_type'][0]]) + '\''
+			insert += ' WHERE resourceID = ' + item['ID'] + ';\n'
+		else :
+			logging.error('This access doesn\'t exist : ' + item['access_type'][0] + ' in item ' + item['ID'] + '.')
+	else :
+		logging.error('The item length is not correct for item ' + item['ID'] + '\n')
 	return insert
 
 def writeFile(data) :
@@ -42,13 +113,43 @@ def writeFile(data) :
 	f.write(data)
 
 def main() :
-	# load json file with data in french
+	# Logging initiation routine
+	log_folder = 'log'
+	log_level = logging.DEBUG
+
+	# Create log file path
+	log_file = os.path.join(log_folder, sys.argv[0].replace('.py', '.log'))
+
+	# Init logs
+	logging.basicConfig(filename = log_file, filemode = 'w+', format = '%(asctime)s  |  %(levelname)s  |  %(message)s', datefmt = '%m/%d/%Y %I:%M:%S %p', level = log_level)
+	logging.info('Start')
+
+	# empty the resources and languages tables
+	result = ''
+	result += 'TRUNCATE TABLE ' + resource_table_name + ';\n'
+	result += 'TRUNCATE TABLE ' + lang_table_name + ';\n'
+	result += 'TRUNCATE TABLE ResourceNote;\n'
+	result += 'TRUNCATE TABLE ResourceOrganizationLink;\n'
+	result += 'TRUNCATE TABLE ResourceRelationship;\n'
+	result += 'TRUNCATE TABLE ResourceTuto;\n'
+	result += 'TRUNCATE TABLE ResourceTypeLink;\n'
+	result += 'TRUNCATE TABLE ResourceSubject;\n'
+
+	# load json file with data in english
 	with open(json_en_file) as json_file :
 		items = json.load(json_file)['items']
-	result = ''
 	for item in items :
-		result += insertItem(item)
-	writeFile(result)
+		result += insertEnglishItem(item)
+
+	# load json file with data in french
+	with open(json_fr_file) as json_file :
+		items = json.load(json_file)['items']
+	for item in items :
+		result += insertFrenchItem(item)
+
+	# Write SQL query in file
+	writeFile(result.encode('utf-8'))
+	logging.info('End')
 
 
 #
